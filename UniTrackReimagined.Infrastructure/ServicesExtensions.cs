271a2a58cecce62ed548e542fae9ffd1a7f25c5f
Spas Milenkov <spasmilenkov123@gmail.com>
2024-04-14 18:35:47 +0300

    Add service collection extension for abstracting away server dependency injection

--- UniTrackReimagined.Infrastructure/ServicesExtensions.cs
+++ UniTrackReimagined.Infrastructure/ServicesExtensions.cs
@@ -1,6 +1,34 @@
+using Mappings;
+using Messaging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UniTrackBackend.Services;
+using UniTrackReimagined.Data;
+using UniTrackReimagined.Services.Authentication;
+
 namespace Infrastructure;
 
 public static class ServicesExtensions
 {
-    
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(c =>
+        {
+            c.AddPolicy("AllowOrigin",
+                options => options.WithOrigins(
+                        "https://localhost:5500",
+                        "http://localhost:5173",
+                        "http://localhost:4200",
+                        "http://localhost")
+                    .AllowCredentials()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+            );
+        });
+        services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IMapper, Mapper>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
+    }
 }
