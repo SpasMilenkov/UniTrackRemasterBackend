0ac2fbf65b64dd26546025a0f414bc777b4a7f29
Spas Milenkov <spasmilenkov123@gmail.com>
2024-10-31 20:13:55 +0200

    feat: add application response dto

---
+++ UniTrackRemaster.Api.Dto/Response/ApplicationResponseDto.cs
@@ -0,0 +1,3 @@
+namespace UniTrackRemaster.Api.Dto.Response;
+
+public record ApplicationResponseDto(string Id, string SchoolId, string ApplicationId, string SchoolName, string FirstName, string LastName, string Еmail);
