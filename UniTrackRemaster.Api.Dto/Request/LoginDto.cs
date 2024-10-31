800bd6e896dd17f45603f647e0b24e9eb09d2f36
Spas Milenkov <spasmilenkov123@gmail.com>
2024-10-31 20:07:51 +0200

    feat: add login and register dtos

--- UniTrackRemaster.Api.Dto/Request/LoginDto.cs
+++ UniTrackRemaster.Api.Dto/Request/LoginDto.cs
@@ -2,14 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UniTrackRemaster.Api.Dto.Request;
 
-public class LoginDto
+public record LoginDto
 {
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email format.")]
     [StringLength(30, ErrorMessage = "Email cannot be longer than 100 characters.")]
-    public required string Email { get; set; }
+    public required string Email { get; init; }
     
     [Required(ErrorMessage = "Password is required.")]
     [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters long.")]
-    public required string Password { get; set; }
+    public required string Password { get; init; }
 }
