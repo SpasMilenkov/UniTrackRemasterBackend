ed52afe0ee85c24449f1265dc53e2a87d13d4f8a
Spas Milenkov <spasmilenkov123@gmail.com>
2024-11-01 23:48:12 +0200

    refactor: make properties which are not needed for initial creation nullable

--- UniTrackRemaster.Data.Models/Organizations/School.cs
+++ UniTrackRemaster.Data.Models/Organizations/School.cs
@@ -3,6 +3,7 @@ using UniTrackRemaster.Data.Models.Academical;
 using UniTrackRemaster.Data.Models.Analytics;
 using UniTrackRemaster.Data.Models.Events;
 using UniTrackRemaster.Data.Models.Images;
+using UniTrackRemaster.Data.Models.Location;
 using UniTrackRemaster.Data.Models.Users;
 
 namespace UniTrackRemaster.Data.Models.Organizations;
@@ -11,24 +12,24 @@ public class School
 {
     #region Properties
 
-    public required Guid Id { get; set; }
+    public Guid Id { get; set; }
     [Length(0, 100)]
     public required string Name { get; set; }
     [Length(0, 255)]
     public string? Description { get; set; }
-    public string Moto { get; set; }
+    public string? Moto { get; set; }
     public IntegrationStatus IntegrationStatus { get; set; }
-    public string Type { get; set; }
-    public string[] Programs { get; set; }
+    public string? Type { get; set; }
+    public string[]? Programs { get; set; }
     #endregion
     #region NavigationProperties
     
     public ICollection<Student>? Students { get; set; }
     public ICollection<Teacher>? Teachers { get; set; }
     public ICollection<Event>? Events { get; set; }
-    public Guid SchoolReportId { get; set; }
+    public Guid? SchoolReportId { get; set; }
     public SchoolReport? SchoolReport { get; set; }
-    public SchoolImage[] Images { get; set; }
-
+    public SchoolImage[]? Images { get; set; }
+    public required SchoolAddress Address { get; set; }
     #endregion
 }
