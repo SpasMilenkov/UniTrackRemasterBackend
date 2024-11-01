d694b917a2f39b2756d4df772ff3a4018859a979
Spas Milenkov <spasmilenkov123@gmail.com>
2024-11-01 23:44:06 +0200

    feat: add application response dto

--- UniTrackRemaster.Api.Dto/Response/ApplicationResponseDto.cs
+++ UniTrackRemaster.Api.Dto/Response/ApplicationResponseDto.cs
@@ -1,3 +1,28 @@
+using UniTrackRemaster.Api.Dto.Request;
+using UniTrackRemaster.Data.Models.Organizations;
+
 namespace UniTrackRemaster.Api.Dto.Response;
 
-public record ApplicationResponseDto(string Id, string SchoolId, string ApplicationId, string SchoolName, string FirstName, string LastName, string Еmail);
+public record class ApplicationResponseDto
+{
+    public required string Id { get; init; }
+    public required string SchoolId { get; init; }
+    public required string SchoolName { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+
+    // Static factory method to map from Application entity to ApplicationResponseDto
+    public static ApplicationResponseDto FromEntity(Application application)
+    {
+        return new ApplicationResponseDto
+        {
+            Id = application.Id.ToString(),
+            SchoolId = application.SchoolId.ToString(),
+            SchoolName = application?.School?.Name ?? throw new InvalidOperationException(message: "School value is null for application"),        // Assuming School is a navigation property
+            FirstName = application.FirstName,
+            LastName = application.LastName,
+            Email = application.Email
+        };
+    }
+}
