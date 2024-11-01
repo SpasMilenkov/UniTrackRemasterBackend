184dc515f5eb94f5d4c4579cdefd7986d976ca63
Spas Milenkov <spasmilenkov123@gmail.com>
2024-11-01 23:45:44 +0200

    feat: add school repository

--- UniTrackRemaster.Data.Commons/ISchoolRepository.cs
+++ UniTrackRemaster.Data.Commons/ISchoolRepository.cs
@@ -1,12 +1,15 @@
 using UniTrackRemaster.Api.Dto.Request;
+using UniTrackRemaster.Data.Models.Location;
 using UniTrackRemaster.Data.Models.Organizations;
 
 namespace UniTrackRemaster.Commons;
 
 public interface ISchoolRepository
 {
-    Task<School> CreateSchoolAsync(CreateSchoolDto createDto);
+    Task<School> CreateSchoolAsync(string name, SchoolAddress address);
+    Task<School> InitSchoolAsync(InitSchoolDto initDto);
     Task<School> GetSchoolAsync(Guid schoolId);
+    Task<List<School>> GetSchoolsAsync(int pageNumber, int pageSize);
     Task<School> UpdateSchoolAsync(UpdateSchoolDto updateDto);
     Task DeleteSchoolAsync(Guid schoolId);
 }
--- UniTrackRemaster.Data.Repositories/SchoolRepository.cs
+++ UniTrackRemaster.Data.Repositories/SchoolRepository.cs
@@ -2,24 +2,30 @@ using Microsoft.EntityFrameworkCore;
 using UniTrackRemaster.Api.Dto.Request;
 using UniTrackRemaster.Commons;
 using UniTrackRemaster.Data.Context;
+using UniTrackRemaster.Data.Models.Location;
 using UniTrackRemaster.Data.Models.Organizations;
 
 namespace UniTrackRemaster.Data.Repositories;
 
 public class SchoolRepository(UniTrackDbContext context) : ISchoolRepository
 {
-    public async Task<School> CreateSchoolAsync(CreateSchoolDto createDto)
+    public async Task<School> CreateSchoolAsync(string schoolName, SchoolAddress address)
     {
         var school = new School
         {
-            Id = default,
-            Name = createDto.Name,
+            Address = address,
+            Name = schoolName,
         };
         await context.Schools.AddAsync(school);
         await context.SaveChangesAsync();
         return school;
     }
-    
+
+    public Task<School> InitSchoolAsync(InitSchoolDto initDto)
+    {
+        throw new NotImplementedException();
+    }
+
     public async Task<School> GetSchoolAsync(Guid schoolId)
     {
         var school = await context.Schools.FindAsync(schoolId);
@@ -28,10 +34,12 @@ public class SchoolRepository(UniTrackDbContext context) : ISchoolRepository
         return school;
     }
 
-    public async Task<List<School>> GetSchoolsAsync()
+    public async Task<List<School>> GetSchoolsAsync(int pageNumber, int pageSize)
     {
-        var schools = await context.Schools.ToListAsync();
-        return schools;
+        return await context.Schools
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
     }
     
     public async Task<School> UpdateSchoolAsync(UpdateSchoolDto updateDto)
