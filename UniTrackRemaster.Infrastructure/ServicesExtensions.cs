30f45b724e1f820c688ac4a69a58238c869e8afe
Spas Milenkov <spasmilenkov123@gmail.com>
2024-11-01 23:48:34 +0200

    chore: update deps

--- UniTrackRemaster.Infrastructure/ServicesExtensions.cs
+++ UniTrackRemaster.Infrastructure/ServicesExtensions.cs
@@ -1,8 +1,11 @@
 using UniTrackRemaster.Mappings;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OrganizationServices;
 using StorageService;
+using UniTrackRemaster.Commons;
 using UniTrackRemaster.Data;
+using UniTrackRemaster.Data.Repositories;
 using UniTrackRemaster.Messaging;
 using UniTrackRemaster.Services.Authentication;
 
@@ -19,6 +22,7 @@ public static class ServicesExtensions
                         "https://localhost:5500",
                         "http://localhost:5173",
                         "http://localhost:4200",
+                        "http://localhost:3000",
                         "http://localhost")
                     .AllowCredentials()
                     .AllowAnyMethod()
@@ -29,6 +33,10 @@ public static class ServicesExtensions
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IMapper, Mapper>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IApplicationRepository, ApplicationRepository>();
+        services.AddScoped<IApplicationService, ApplicationService>();
+        services.AddScoped<ISchoolRepository, SchoolRepository>();
+        services.AddScoped<ISchoolService, SchoolService>();
         services.AddSingleton<IFirebaseStorageService>(provider => 
             new FirebaseStorageService(
                 configuration.GetSection("FirebaseStorage").Value ?? string.Empty,
