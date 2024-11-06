21e75d60a3ad8530c3e09096b60ee20de12c2915
Spas Milenkov <spasmilenkov123@gmail.com>
2024-11-06 03:35:23 +0200

    chore: add migrations

---
+++ UniTrackRemaster.Data/Migrations/20241105193434_AddCodeColumnToApplication.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniTrackRemaster.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeColumnToApplication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Applications",
+                type: "text", // Adjust the type as needed for your column type
+                nullable: true); // Adjust nullability if necessary
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Applications");
+        }
+    }
+}
