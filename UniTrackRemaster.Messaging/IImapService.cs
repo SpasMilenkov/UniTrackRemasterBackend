2b8a488a09b4e6cb30fbb7a8b49b1a47e4846fbf
Spas Milenkov <spasmilenkov123@gmail.com>
2025-01-13 22:02:32 +0200

    feat: add IMAP service with Gmail integration

---
+++ UniTrackRemaster.Messaging/IImapService.cs
@@ -0,0 +1,17 @@
+using MailKit;
+using MailKit.Net.Imap;
+using MimeKit;
+
+namespace UniTrackRemaster.Messaging;
+
+
+public interface IImapService
+{
+    Task<IList<MimeMessage>> GetRecentEmailsAsync(int count = 10);
+    Task<IList<MimeMessage>> GetUnprocessedEmailsAsync(int count = 10);
+    Task<MimeMessage> GetEmailByIdAsync(string messageId);
+    Task<IList<MimeMessage>> SearchEmailsAsync(string searchTerm);
+    Task<IList<MimeMessage>> GetEmailsByDateRangeAsync(DateTime start, DateTime end);
+    Task MarkEmailAsProcessedAsync(string messageId);
+    Task MoveEmailToFolderAsync(string messageId, string folderName);
+}
