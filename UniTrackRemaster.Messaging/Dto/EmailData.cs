86db9e0e31317989522766d0e698e45e6653f333
Spas Milenkov <spasmilenkov123@gmail.com>
2025-01-13 22:06:11 +0200

    feat: add email data dto

---
+++ UniTrackRemaster.Messaging/Dto/EmailData.cs
@@ -0,0 +1,10 @@
+namespace UniTrackRemaster.Messaging.Dto;
+
+public class EmailData
+{
+    public string Subject { get; set; } = string.Empty;
+    public string From { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string Body { get; set; } = string.Empty;
+    public string MessageId { get; set; } = string.Empty;
+}
