62ea67eb786240ab10c4a34a2ad5764950f5558d
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 18:00:41 +0200

    refactor(faculty): add code, website, contact email, contact phone, university id, university, majors and departments properties

--- UniTrackRemaster.Data.Models/Academical/Faculty.cs
+++ UniTrackRemaster.Data.Models/Academical/Faculty.cs
@@ -1,16 +1,23 @@
+using UniTrackRemaster.Data.Models.Enums;
+using UniTrackRemaster.Data.Models.Organizations;
+
 namespace UniTrackRemaster.Data.Models.Academical;
 
-public class Faculty: BaseEntity
+public class Faculty : BaseEntity
 {
-    #region Properties
-
     public Guid Id { get; set; }
     public required string Name { get; set; }
+    public required string Code { get; set; }
     public required string ShortDescription { get; set; }
     public required string DetailedDescription { get; set; }
-    #endregion
-    #region NavigationProperties
+    public string? Website { get; set; }
+    public string? ContactEmail { get; set; }
+    public string? ContactPhone { get; set; }
+    public required FacultyStatus Status { get; set; }
     
-    public ICollection<Major>? Majors { get; set; }
-    #endregion
+    public required Guid UniversityId { get; set; }
+    public University University { get; set; }
+    
+    public IList<Major>? Majors { get; set; }
+    public IList<Department>? Departments { get; set; }
 }
