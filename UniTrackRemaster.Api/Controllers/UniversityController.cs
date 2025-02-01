9db494452d3799fd36d895f8d84b8c403a3a2e0b
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 20:01:10 +0200

    feat: add university controller

---
+++ UniTrackRemaster.Api/Controllers/UniversityController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using OrganizationServices;
+using UniTrackRemaster.Api.Dto.Institution;
+using UniTrackRemaster.Api.Dto.Request;
+
+namespace UniTrackRemaster.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UniversitiesController : ControllerBase
+{
+    private readonly IUniversityService universityService;
+
+    public UniversitiesController(IUniversityService universityService)
+    {
+        this.universityService = universityService;
+    }
+
+    [HttpPost("init")]
+    public async Task<IActionResult> InitUniversity(
+        [FromForm] InitUniversityDto universityData,
+        [FromForm] UniversityFilesModel files)
+    {
+        var universityId = await universityService.InitUniversityAsync(universityData, files.Logo, files.AdditionalImages);
+
+        return Ok(new { 
+            Message = "University created successfully"
+        });
+    }
+}
