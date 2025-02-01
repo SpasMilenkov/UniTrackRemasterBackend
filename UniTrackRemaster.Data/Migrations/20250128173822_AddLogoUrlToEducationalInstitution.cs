52dd45093fcdaeb268dcfd2b5a7df0a76fe39db2
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 19:53:06 +0200

    feat(migrations): add logo url common to the institutions table

---
+++ UniTrackRemaster.Data/Migrations/20250128173822_AddLogoUrlToEducationalInstitution.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniTrackRemaster.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLogoUrlToEducationalInstitution : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrl",
+                table: "EducationalInstitutions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoUrl",
+                table: "EducationalInstitutions");
+        }
+    }
+}
