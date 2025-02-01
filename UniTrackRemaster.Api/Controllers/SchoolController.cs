485b145f30ba5ec5edb2aa7b6f587c7e3ccfc3cc
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 20:00:33 +0200

    refactor: remove logging logic on init, fix image uploading to firebase

--- UniTrackRemaster.Api/Controllers/SchoolController.cs
+++ UniTrackRemaster.Api/Controllers/SchoolController.cs
@@ -1,44 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using OrganizationServices;
 using StorageService;
+using UniTrackRemaster.Api.Dto.Institution;
 using UniTrackRemaster.Api.Dto.Request;
 
 namespace UniTrackRemaster.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SchoolController(ISchoolService schoolService, IFirebaseStorageService storageService, ISchoolImageService schoolImageService) : ControllerBase
+    public class SchoolController(ISchoolService schoolService) : ControllerBase
     {
+
         [HttpPost("init")]
         public async Task<IActionResult> InitSchool(
             [FromForm] InitSchoolDto schoolData,
-            [FromForm] List<IFormFile> files)
+            [FromForm] SchoolFilesModel files)
         {
-            var schoolId = await schoolService.InitSchoolAsync(schoolData);
-            // Log or process the schoolData if needed
-            Console.WriteLine($"School Name: {schoolData.Name}");
+            var schoolId = await schoolService.InitSchoolAsync(schoolData, files.Logo, files.AdditionalImages);
 
-            var fileUrls = new List<string>();
-
-            // Upload each file to Firebase and store the URLs
-            foreach (var file in files)
-            {
-                if (file.Length <= 0) continue;
-                var fileUrl = await storageService.UploadFileAsync(file);
-                fileUrls.Add(fileUrl); // Collect each file's URL for response or processing
-            }
-
-            await schoolImageService.AddBulkAsync(fileUrls, schoolId);
-            
-            // Return success response, including uploaded file URLs if needed
-            return Ok(new { Message = "School created successfully", FileUrls = fileUrls });
+            return Ok(new { 
+                Message = "School created successfully"
+            });
         }
 
         [HttpGet("list")]
-        public async Task<IActionResult> GetSchools(int page = 1, int pageSize = 10)
+        public async Task<IActionResult> GetSchools([FromQuery] SchoolFilterDto filter)
         {
-            var schools = await schoolService.GetSchoolsAsync(page, pageSize);
+            var schools = await schoolService.GetSchoolsAsync(filter);
             return Ok(schools);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSchool(Guid id)
+        {
+            var school = await schoolService.GetSchoolAsync(id);
+            return Ok(school);
         }
     }
 }
