4d80b2a5b5e232aa9b81fbb7292ad80b6a2ccb67
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 19:48:34 +0200

    chore: remove obsolete mapper project

--- UniTrackRemaster.Mappings/IMapper.cs
+++
@@ -1,9 +0,0 @@
-using UniTrackRemaster.Api.Dto.Response;
-using UniTrackRemaster.Data.Models.Academical;
-
-namespace UniTrackRemaster.Mappings;
-
-public interface IMapper
-{
-    public LoginResponseDto? MapLoginResult(Guid userId, string role);
-}
