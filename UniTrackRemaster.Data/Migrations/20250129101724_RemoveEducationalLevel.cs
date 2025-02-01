f63527798275b53912266aa99447a98e26d1ae62
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 19:54:47 +0200

    feat(migrations): remove educational level

---
+++ UniTrackRemaster.Data/Migrations/20250129101724_RemoveEducationalLevel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniTrackRemaster.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEducationalLevel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Levels",
+                table: "Universities");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Schools");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "Levels",
+                table: "Universities",
+                type: "integer[]",
+                nullable: false,
+                defaultValue: new int[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Level",
+                table: "Schools",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
