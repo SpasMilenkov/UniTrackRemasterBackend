112d5a8574a3614077442063c755e2b58bcce1a9
Spas Milenkov <spasmilenkov123@gmail.com>
2025-02-01 18:57:20 +0200

    feat: add xml documents generation

--- UniTrackRemaster.Infrastructure/SwaggerExtensions.cs
+++ UniTrackRemaster.Infrastructure/SwaggerExtensions.cs
@@ -1,6 +1,9 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Infrastructure;
 
@@ -22,7 +25,17 @@ public static class SwaggerExtensions
                 },
                 Version = "v1"
             });
-            
+        
+            var infrastructureXmlFile = "UniTrackRemaster.Infrastructure.xml";
+        
+            var apiXmlPath = Path.Combine(Directory.GetCurrentDirectory(), $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+            var infrastructureXmlPath = Path.Combine(Directory.GetCurrentDirectory(), infrastructureXmlFile);
+
+            // Include XML comments from both projects
+            options.IncludeXmlComments(apiXmlPath);
+            options.IncludeXmlComments(infrastructureXmlPath);
+
+        
             options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
             {
                 Description = "Standard authorization header using bearer token",
@@ -31,7 +44,6 @@ public static class SwaggerExtensions
                 Type = SecuritySchemeType.ApiKey
             });
             options.OperationFilter<SecurityRequirementsOperationFilter>();
-            
         });
     }
 }
