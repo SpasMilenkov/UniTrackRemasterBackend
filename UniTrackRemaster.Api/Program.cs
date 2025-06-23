242edd147467412b5e08d46ec46276922a400067
Spas Milenkov <spasmilenkov123@gmail.com>
2025-06-24 00:10:15 +0300

    feat: add rate limiting, metrics monitoring and signalR hub mapping

--- UniTrackRemaster.Api/Program.cs
+++ UniTrackRemaster.Api/Program.cs
@@ -1,7 +1,11 @@
 using System.Text.Json.Serialization;
+using AspNetCoreRateLimit;
 using Infrastructure;
+using Microsoft.AspNetCore.Http.Connections;
 using Prometheus;
 using UniTrackRemaster.Data.Seeding;
+using UniTrackRemaster.EventHandlers;
+using UniTrackRemaster.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,14 +18,19 @@ builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddAuthorization();
 builder.Services.AddJwtToken(builder.Configuration);
 builder.Services.AddServices(builder.Configuration);
+builder.Services.AddOpenTelemetry(builder.Configuration);
+builder.Services.AddMetricsServices();
+builder.Services.ConfigureSignalR(builder.Configuration, builder.Environment);
 builder.Services.AddSwagger();
+builder.Services.AddHostedService<ChatSignalREventHandler>();
 // builder.WebHost.UseKestrel()
 //                .UseUrls("http://*:5086");
 builder.Services.AddHealthChecks();
@@ -35,22 +44,28 @@ try
     app.UseCors("AllowOrigin");
     app.UseAuthentication();
     app.UseAuthorization();
+    app.UseWebSockets();
+    app.MapHub<ChatHub>("/hubs/chat", options =>
+    {
+        options.Transports = HttpTransportType.WebSockets | HttpTransportType.ServerSentEvents | HttpTransportType.LongPolling;
+    });
     app.MapControllers();
     app.UseMetricServer();
-    app.UseHttpMetrics(); 
+    app.UseHttpMetrics();
+    app.UseIpRateLimiting();
     CookieOptionManager.Initialize(builder.Configuration);
-    await DataSeeder.SeedData(app.Services);
+
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
     }
-    
+
     app.Lifetime.ApplicationStopping.Register(() =>
     {
         Console.WriteLine("Application is stopping");
     });
-
+    app.UseMetricsMiddleware();
     app.Run();
 }
 catch (Exception ex)
