3b26ccd57ce01979ea292a3993f30224436a6ddf
Spas Milenkov <spasmilenkov123@gmail.com>
2025-06-23 23:15:45 +0300

    feat: update event scheduling entities with expanded tracking

--- UniTrackRemaster.Data.Models/Events/Event.cs
+++ UniTrackRemaster.Data.Models/Events/Event.cs
@@ -1,26 +1,46 @@
 using System.ComponentModel.DataAnnotations;
+using UniTrackRemaster.Data.Models.Enums;
+using UniTrackRemaster.Data.Models.Organizations;
 
 namespace UniTrackRemaster.Data.Models.Events;
 
-public class Event: BaseEntity
+public class Event : BaseEntity
 {
     #region Properties
-        public required Guid Id { get; set; }
-        [Length(0, 255)]
-        public required string Title { get; set; }
-        [Length(0, 255)]
-        public required string Topic { get; set; }
-        [Length(0, 255)]
-        public string? Description { get; set; }
+    public required Guid Id { get; set; }
 
+    [Length(0, 255)]
+    public required string Title { get; set; }
+
+    [Length(0, 255)]
+    public required string Topic { get; set; }
+
+    [Length(0, 500)]
+    public string? Description { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string? Location { get; set; }
+    public EventType Type { get; set; }
+    public EventStatus Status { get; set; } = EventStatus.Scheduled;
+    public bool IsAllDay { get; set; }
+    public bool IsRecurring { get; set; }
+    public RecurrencePattern? RecurrencePattern { get; set; }
+    public int? MaxParticipants { get; set; }
+    public bool RequiresApproval { get; set; }
+    public string? MeetingLink { get; set; }
+    public string? Notes { get; set; }
     #endregion
+
     #region NavigationProperties
+    public required Guid OrganizerId { get; set; }
+    public Organizer? Organizer { get; set; }
 
-        public required Guid OrganizerId { get; set; }
-        public Organizer? Organizer { get; set; }
-        public required IList<Attender> Attenders { get; set; }
-        public required IList<Participant> Participants { get; set; }
+    public Guid? InstitutionId { get; set; }
+    public Institution? Institution { get; set; }
 
-
+    public IList<Attender> Attenders { get; set; } = new List<Attender>();
+    public IList<Participant> Participants { get; set; } = new List<Participant>();
+    public IList<EventNotification> Notifications { get; set; } = new List<EventNotification>();
     #endregion
 }
--- UniTrackRemaster.Data.Models/Events/Participant.cs
+++ UniTrackRemaster.Data.Models/Events/Participant.cs
@@ -1,18 +1,23 @@
+using UniTrackRemaster.Data.Models.Enums;
 using UniTrackRemaster.Data.Models.Users;
 
 namespace UniTrackRemaster.Data.Models.Events;
 
-public class Participant: BaseEntity
+public class Participant : BaseEntity
 {
     #region Properties
-
     public required Guid Id { get; set; }
-
+    public ParticipantStatus Status { get; set; } = ParticipantStatus.Invited;
+    public DateTime? ResponseDate { get; set; }
+    public string? ResponseNote { get; set; }
+    public bool IsRequired { get; set; }
     #endregion
-    #region NavigationProperties
 
+    #region NavigationProperties
     public required Guid UserId { get; set; }
     public ApplicationUser? User { get; set; }
 
+    public required Guid EventId { get; set; }
+    public Event? Event { get; set; }
     #endregion
 }
