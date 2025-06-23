fad1548d96111743d252b8b67b2a6c3b1d3b803b
Spas Milenkov <spasmilenkov123@gmail.com>
2025-06-23 23:17:31 +0300

    refactor: add privacy columns to user, rework the pattern to properly integrate with the new hierarchy, rework teacher student and admin to work without the junction entity

--- UniTrackRemaster.Data.Models/Users/Parent.cs
+++ UniTrackRemaster.Data.Models/Users/Parent.cs
@@ -1,18 +1,22 @@
+using UniTrackRemaster.Data.Models.Enums;
+using UniTrackRemaster.Data.Models.Organizations;
+
 namespace UniTrackRemaster.Data.Models.Users;
 
-public class Parent: BaseEntity
+public class Parent : BaseEntity
 {
     #region Properties
-
-        public required Guid Id { get; set; }
-
+    public required Guid Id { get; set; }
+    public string? Occupation { get; set; }
+    public string? EmergencyContact { get; set; }
+    public ProfileStatus Status { get; set; } = ProfileStatus.Pending;
+    public string? Notes { get; set; }
     #endregion
     
     #region NavigationProperties
-        public required Guid UserId { get; set; }
-        public ApplicationUser? User { get; set; }
-        
-        public required IList<Student> Children {get; set;}
-
+    public required Guid UserId { get; set; }
+    public ApplicationUser? User { get; set; }
+    
+    public required IList<Student> Children { get; set; } = new List<Student>();
     #endregion
 }
--- UniTrackRemaster.Data.Models/Users/Teacher.cs
+++ UniTrackRemaster.Data.Models/Users/Teacher.cs
@@ -1,43 +1,46 @@
 using System.ComponentModel.DataAnnotations;
 using UniTrackRemaster.Data.Models.Academical;
+using UniTrackRemaster.Data.Models.Enums;
 using UniTrackRemaster.Data.Models.Events;
-using UniTrackRemaster.Data.Models.JunctionEntities;
 using UniTrackRemaster.Data.Models.Organizations;
 
 namespace UniTrackRemaster.Data.Models.Users;
 
-public class Teacher: BaseEntity
+public class Teacher : BaseEntity
 {
     #region Properties
-    
-        [Length(0, 50)]
-        public string? Title { get; set; }
-        
-        //PrimaryKey
-        public required Guid Id { get; set; }
-        
+
+    [Length(0, 50)]
+    public string? Title { get; set; }
+
+    //PrimaryKey
+    public required Guid Id { get; set; }
+
+    public ProfileStatus Status { get; set; } = ProfileStatus.Pending;
+
     #endregion
 
     #region NavigationProperties
 
-        //User relation
-        public required Guid UserId { get; set; }
-        public ApplicationUser? User { get; set; }
+    //User relation
+    public required Guid UserId { get; set; }
+    public ApplicationUser? User { get; set; }
 
-        //Events relation
-        public IList<Event>? Events { get; set; }
+    //Events relation
+    public IList<Event>? Events { get; set; }
 
-        //School relations
-        public Guid InstitutionId { get; set; }
-        public Institution? Institution { get; set; }
+    //School relations
+    public Guid InstitutionId { get; set; }
+    public Institution? Institution { get; set; }
 
-        //Class grade relation
-        public Guid? ClassGradeId { get; set; }
-        public Grade? ClassGrade { get; set; }
-        
-        public IList<HomeRoomTeacher> HomeRoomAssignments { get; set; }
-        public IList<Club> SupervisedClubs { get; set; }
-        public IList<ElectiveSubject> ElectiveSubjects { get; set; }
+    //Class grade relation
+    public Guid? ClassGradeId { get; set; }
+    public Grade? ClassGrade { get; set; }
+
+    public IList<HomeRoomTeacher>? HomeRoomAssignments { get; set; }
+    public IList<ElectiveSubject>? ElectiveSubjects { get; set; }
+    public IList<Grade>? Grades { get; set; }
+    public IList<Subject>? Subjects { get; set; }
 
     #endregion
 
